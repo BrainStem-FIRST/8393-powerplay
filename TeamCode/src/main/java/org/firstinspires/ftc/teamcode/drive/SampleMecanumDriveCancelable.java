c5d91d2b517e6d47a1b77964f7dda0e8222d4a66
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-11-11 18:53:07 -0500

    pidf updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDriveCancelable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDriveCancelable.java
@@ -53,8 +53,8 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDriveCancelable extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(7, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(3, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(60, 0, 0);
 
     public static double LATERAL_MULTIPLIER = 1;
 
