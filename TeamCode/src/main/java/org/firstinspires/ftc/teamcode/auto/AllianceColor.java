9e1a754f42c91a2de563bc97d82d9af2448bf615
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-11-16 22:30:47 -0500

    Revert "Merge pull request #3 from BrainStem-FIRST/code-organization"
    This reverts commit dcbaebbd016a78b64b8369c0e52a3dd82ea56daf, reversing
    changes made to 881233aa921d73775411673fc320661f19bf4d13.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AllianceColor.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.auto;
-
-public enum AllianceColor {
-    RED, BLUE
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BarcodePattern.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.auto;
-
-public enum BarcodePattern {
-    LEFT, CENTER, RIGHT
-}
