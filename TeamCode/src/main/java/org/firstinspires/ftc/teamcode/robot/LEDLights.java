543a92162d8148a596ac6e14594b180338b9c6eb
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-11-25 15:57:58 -0500

    LED LIGHTS!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/LEDLights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/LEDLights.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class LEDLights {
 
+
+    public LEDLights (HardwareMap hwMap, Telemetry telemetry) {
+
+    }
 
 
 
