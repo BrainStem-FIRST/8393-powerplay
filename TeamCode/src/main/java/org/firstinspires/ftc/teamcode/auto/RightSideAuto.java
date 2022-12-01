5b612f3e742bdb930aedf996ea4d130feaf75105
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-11-30 21:58:11 -0500

    name refactor for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="1 - RIGHT SIDE AUTO")
-public class BlueAuto2 extends Auto2 {
-    public BlueAuto2() {super(Auto2.AllianceColor.BLUE);}
+public class RightSideAuto extends Auto2 {
+    public RightSideAuto() {super(Auto2.AllianceColor.BLUE);}
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
