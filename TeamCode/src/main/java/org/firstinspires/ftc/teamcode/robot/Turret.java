412896a126a0390e5f320c7493ab787a92793361
prishadargan <prisha.dargan@gmail.com>
2022-11-30 20:19:15 -0500

    updated pwms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Turret.java
@@ -24,12 +24,12 @@ public class Turret {
     public final String     LEFT_POSITION = "LEFT_STATE";
     public final String     RIGHT_POSITION = "RIGHT_STATE";
     public final String     CENTER_POSITION = "CENTER_STATE";
-    public final double     LEFT_POSITION_LEFT_SERVO_VALUE = 349;
-    public final double     LEFT_POSITION_RIGHT_SERVO_VALUE = 349;
-    public final double     CENTER_POSITION_LEFT_SERVO_VALUE = 1282;
-    public final double     CENTER_POSITION_RIGHT_SERVO_VALUE = 1282;
-    public final double     RIGHT_POSITION_LEFT_SERVO_VALUE = 2215;
-    public final double     RIGHT_POSITION_RIGHT_SERVO_VALUE = 2215;
+    public final double     LEFT_POSITION_LEFT_SERVO_VALUE = 549;
+    public final double     LEFT_POSITION_RIGHT_SERVO_VALUE = 549;
+    public final double     CENTER_POSITION_LEFT_SERVO_VALUE = 1373;
+    public final double     CENTER_POSITION_RIGHT_SERVO_VALUE = 1373;
+    public final double     RIGHT_POSITION_LEFT_SERVO_VALUE = 2155;
+    public final double     RIGHT_POSITION_RIGHT_SERVO_VALUE = 2155;
     public final int        LIFT_MIN_HEIGHT_TO_MOVE_TURRET = 75;
 
     public Telemetry telemetry;
@@ -65,13 +65,13 @@ public class Turret {
     public void selectTransition(String desiredLevel){
         switch(desiredLevel){
             case LEFT_POSITION:{
-                transitionToPosition(0.025, 0.0025);
+                transitionToPosition(0, 0);
                 break;
             } case CENTER_POSITION:{
-                transitionToPosition(0.55, 0.55);
+                transitionToPosition(0.5, 0.5);
                 break;
             } case RIGHT_POSITION:{
-                transitionToPosition(0.975, 0.975);
+                transitionToPosition(1, 1);
                 break;
             }
         }
