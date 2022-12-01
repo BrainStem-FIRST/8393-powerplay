ee82918d248b6d9a71628bee40005b40852e7d2e
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-11-30 21:57:28 -0500

    name refactor for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto2.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="2 Blue Auto")
+@Autonomous(name="1 - RIGHT SIDE AUTO")
 public class BlueAuto2 extends Auto2 {
     public BlueAuto2() {super(Auto2.AllianceColor.BLUE);}
     @Override
