8becd4b8fd5dbf977fc79feefa52d757727d6df4
Dave Whitmer <dave.whitmer@borrowworks.com>
2022-12-01 15:57:05 -0500

    DW Auto with low pole and 1 high pole

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAutoDw.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="1 - RIGHT SIDE AUTO - DW")
+public class RightSideAutoDw extends Auto2Dw {
+    public RightSideAutoDw() {super(AllianceColor.BLUE);}
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Grabber.java
@@ -41,6 +41,7 @@ public class Grabber {
         grabber = new CachingServo(hwMap.get(ServoImplEx.class, "grabber"));
 
         grabber.setPwmRange(new PwmControl.PwmRange(REGULAR_OPEN, CLOSED_VALUE));
+        stateMap.put(constants.GRABBER_MODE, constants.DEPOSITING);
 
     }
 
@@ -79,7 +80,7 @@ public class Grabber {
     public void close(){grabber.setPosition(1);}
 
     public boolean shouldGrab(Lift lift) {
-        return lift.getPosition() < lift.LIFT_POSITION_GROUND &&
+        return (lift.getPosition() < lift.LIFT_POSITION_GROUND) &&
                 ((String)stateMap.get(constants.CONE_CYCLE)).equalsIgnoreCase(constants.STATE_IN_PROGRESS);
     }
 }
