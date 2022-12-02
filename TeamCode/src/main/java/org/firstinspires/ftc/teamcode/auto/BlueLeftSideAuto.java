a620d8ffbf4fff2d53334ea7db2ac3fb46c253f2
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-12-01 22:00:38 -0500

    Left Side auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueLeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueLeftSideAuto.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "1 Red Auto")
-public class RedAuto1 extends Auto1 {
-    public RedAuto1() {super(AllianceColor.RED);}
+@Autonomous(name = "1 - LEFT SIDE AUTO")
+public class BlueLeftSideAuto extends LeftSideAuto {
+    public BlueLeftSideAuto() {super(AllianceColor.BLUE);}
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedLeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedLeftSideAuto.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "1 Blue Auto")
-public class BlueAuto1 extends Auto1 {
-    public BlueAuto1() {super(AllianceColor.BLUE);}
+@Autonomous(name = "1 Red Auto")
+public class RedLeftSideAuto extends LeftSideAuto {
+    public RedLeftSideAuto() {super(AllianceColor.RED);}
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
