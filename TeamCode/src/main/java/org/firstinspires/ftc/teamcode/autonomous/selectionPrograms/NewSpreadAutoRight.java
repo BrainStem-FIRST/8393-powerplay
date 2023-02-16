00c7323422761ac84aaa499cb7a9abe070978a56
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2023-02-15 21:23:03 -0500

    spread auto progress

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/NewSpreadAutoRight.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autonomous.selectionPrograms;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.NewSpreadAuto;
+
+@Autonomous(name="0 - New Spread Auto")
+public class NewSpreadAutoRight extends NewSpreadAuto {
+    public NewSpreadAutoRight() {super(AutoOrientation.RIGHT);}
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+    }
+}
+
