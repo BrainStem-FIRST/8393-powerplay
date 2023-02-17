c2bf691bf3f07fee37bbd4ee352c908adfe686dc
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2023-02-16 22:17:10 -0500

    logic fix - most up to date code as of rn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/LowPoleRightSideOnePlusFive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/LowPoleRightSideOnePlusFive.java
@@ -1,14 +1,16 @@
-//package org.firstinspires.ftc.teamcode.autonomous.selectionPrograms;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//
-//import org.firstinspires.ftc.teamcode.autonomous.LowPoleOnePlusFive;
-//
-//@Autonomous(name="3 - Right Low 1+5")
-//public class LowPoleRightSideOnePlusFive extends LowPoleOnePlusFive {
-//    public LowPoleRightSideOnePlusFive() {super(AutoOrientation.RIGHT);}
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        super.runOpMode();
-//    }
-//}
+package org.firstinspires.ftc.teamcode.autonomous.selectionPrograms;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.autonomous.LowPoleOnePlusFive;
+
+@Disabled
+@Autonomous(name="3 - Right Low 1+5")
+public class LowPoleRightSideOnePlusFive extends LowPoleOnePlusFive {
+    public LowPoleRightSideOnePlusFive() {super(AutoOrientation.RIGHT);}
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+    }
+}
