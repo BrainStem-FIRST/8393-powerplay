7d12919e1cb95202b952e3ec5c519bf2fa9bff56
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2023-04-14 15:35:28 -0400

    commit in need of rollback

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/UncontestedOnePlusFiveLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/UncontestedOnePlusFiveLeft.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.autonomous.selectionPrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.NewUncontestedOnePlusFive;
 import org.firstinspires.ftc.teamcode.autonomous.UncontestedOnePlusFive;
 
 @Autonomous(name="0 - Left Uncontested 1+5 WIP")
-public class UncontestedOnePlusFiveLeft extends UncontestedOnePlusFive {
+public class UncontestedOnePlusFiveLeft extends NewUncontestedOnePlusFive {
     public UncontestedOnePlusFiveLeft() {super(AutoOrientation.LEFT);}
     @Override
     public void runOpMode() throws InterruptedException {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/UncontestedOnePlusFiveRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/selectionPrograms/UncontestedOnePlusFiveRight.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.autonomous.selectionPrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.NewUncontestedOnePlusFive;
 import org.firstinspires.ftc.teamcode.autonomous.UncontestedOnePlusFive;
 
 @Autonomous(name="0 - Right Uncontested 1+5 WIP")
-public class UncontestedOnePlusFiveRight extends UncontestedOnePlusFive {
+public class UncontestedOnePlusFiveRight extends NewUncontestedOnePlusFive {
     public UncontestedOnePlusFiveRight() {super(AutoOrientation.RIGHT);}
     @Override
     public void runOpMode() throws InterruptedException {
