8f2a06055aaa498bff7b730a1280260e4fba258d
Mihir Sharma <mihirsharma700@gmail.com>
2023-05-28 17:37:01 -0400

    stack 3 adjustment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/teleopSubsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/teleopSubsystems/Lift.java
@@ -40,7 +40,7 @@ public class Lift implements Subsystem {
         //auto stack heights
         private static final int STACK_5_ENCODER_TICKS = 225 + 20 + 5;
         private static final int STACK_4_ENCODER_TICKS = 170 + 10 + 5;
-        private static final int STACK_3_ENCODER_TICKS = 115;
+        private static final int STACK_3_ENCODER_TICKS = 100 + 5;
         private static final int STACK_2_ENCODER_TICKS = 65;
         private static final int STACK_1_ENCODER_TICKS = COLLECTING_ENCODER_TICKS;
 
